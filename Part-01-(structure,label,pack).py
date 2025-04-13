9b11ffc6c953dbc079238ff8c6452349d9201d6a
4Raisan <mn4raisan@gmail.com>
2025-04-13 23:44:17 +0530

    display the label

--- Part-01-(structure,label,pack).py
+++ Part-01-(structure,label,pack).py
@@ -5,4 +5,9 @@ root = Tk() # starting
 # Make label
 label1 = Label(root, text='Hellow World!')
 
+# everything have 2 parts : making & displaying 
+
+# only space wanted, center at every size
+label1.pack()
+
 root.mainloop() # ending
