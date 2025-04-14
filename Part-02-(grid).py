3a5312f92bf300668894296bb74c763fa051ba26
4Raisan <mn4raisan@gmail.com>
2025-04-14 22:42:36 +0530

    more labels

--- Part-02-(grid).py
+++ Part-02-(grid).py
@@ -5,6 +5,10 @@ root = Tk()
 # Make labels
 label1 = Label(root, text='1')
 label2 = Label(root, text='columnspan')
+label3 = Label(root, text='2')
+label4 = Label(root, text='2i')
+label5 = Label(root, text='rowspan')
+label6 = Label(root, text='I am here')
 
 # stay stable, spacing as using
 label1.grid(row=0, column=0)
