0d90f816f81a16ee4f9c1cf1596a511e2c074d49
4Raisan <mn4raisan@gmail.com>
2025-04-15 23:44:04 +0530

    create label

--- Part-03-(Add_button_and_funtions).py
+++ Part-03-(Add_button_and_funtions).py
@@ -2,6 +2,12 @@ from tkinter import *
 
 root = Tk() # starting
 
+# normal function calling with (), with command no () using
+def click():
+    labelb = Label(root, text='Clicked..!')
+    labelb.pack()
+  
+
 # add button
 button1 = Button(root, text='Click Here', command=click, padx=5, pady=2, fg='blue', bg='yellow')
 
