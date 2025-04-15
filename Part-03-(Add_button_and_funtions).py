5d29387f99d8a6d08a41f031e41e6bef27cf5239
4Raisan <mn4raisan@gmail.com>
2025-04-15 23:43:39 +0530

    create button

---
+++ Part-03-(Add_button_and_funtions).py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk() # starting
+
+# add button
+button1 = Button(root, text='Click Here', command=click, padx=5, pady=2, fg='blue', bg='yellow')
+
+root.mainloop() # ending
