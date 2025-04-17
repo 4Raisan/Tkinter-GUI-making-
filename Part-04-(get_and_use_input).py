54e0023c6352bef55c1687f8f953617d78fdb7ca
4Raisan <mn4raisan@gmail.com>
2025-04-17 23:43:23 +0530

    make the input box

--- Part-04-(get_and_use_input).py
+++ Part-04-(get_and_use_input).py
@@ -3,6 +3,6 @@ from tkinter import *
 root = Tk() # starting
 
 # Just create the Input Box  |  # can't use  hight
-
+e = Entry(root, borderwidth=10, width=50, bg='green', fg='black')
 
 root.mainloop() # ending
