60d4f8912dc07ae4feae34f961ef515be2e47e6b
4Raisan <mn4raisan@gmail.com>
2025-04-19 23:46:18 +0530

    use button

--- Part-04-(get_and_use_input).py
+++ Part-04-(get_and_use_input).py
@@ -18,7 +18,7 @@ e.get() # input for using
 def click():
     label1 = Label(root, text='Hi '+e.get()+'...!') # e.get()
     label1.pack()
-    e.delete(0, END) 
+    e.delete(0, END) # clear the box forever after the button clicked
 
 button1 = Button(root, text='Submit')  #, command=click
 button1.pack()
