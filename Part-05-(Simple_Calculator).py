8d536cdcefba8774f9fccc658aeb392b8d142c11
4Raisan <mn4raisan@gmail.com>
2025-04-28 23:50:57 +0530

    sketch the code

--- Part-05-(Simple_Calculator).py
+++ Part-05-(Simple_Calculator).py
@@ -1,6 +1,14 @@
 from tkinter import *
 root = Tk()
 
+# root.title(), like the window name in the close button row
 root.title('Cal : )')
+
+# input
+
+# process as defs (+,-,/,*)
+
+# buttons with positions, use grid()
+
 
 root.mainloop()
