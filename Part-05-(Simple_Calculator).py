43604659c96c989d31e08369d46ec7aa8c371cc7
4Raisan <mn4raisan@gmail.com>
2025-04-29 23:16:25 +0530

    display the button using grids

--- Part-05-(Simple_Calculator).py
+++ Part-05-(Simple_Calculator).py
@@ -13,6 +13,10 @@ e.grid(row=0, column=0)
 # buttons with positions, use grid()
 # numbers
 btnnums = [['1',1,0], ['2',1,1], ['3',1,2], ['4',1,3], ['5',2,0], ['6',2,1], ['7',2,2], ['8',2,3], ['9',3,0], ['0',3,1]]
+for (txt, r, c) in btnnums:
+  btn = Button(root, text=txt)
+  btn.grid(row=r, column=c)
+  
   
 # specials
 
