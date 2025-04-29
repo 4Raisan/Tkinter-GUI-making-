8416a531b07a63725d9afc231c96844364fd96d7
4Raisan <mn4raisan@gmail.com>
2025-04-29 23:25:59 +0530

    add arithmetic operators button

--- Part-05-(Simple_Calculator).py
+++ Part-05-(Simple_Calculator).py
@@ -7,6 +7,7 @@ root.title('Cal : )')
 # input
 e = Entry(root, width=20)
 e.grid(row=0, column=0, columnspan=4)
+#e.pack() use grids to simply design
 
 # process as defs (+,-,/,*)
 
@@ -16,7 +17,9 @@ btnnums = [['1',1,0], ['2',1,1], ['3',1,2], ['4',1,3], ['5',2,0], ['6',2,1], ['7
 for (txt, r, c) in btnnums:
   btn = Button(root, text=txt)
   btn.grid(row=r, column=c)
-  
+# arithmetic operators and .
+btnarm = [['*',4,0], ['/',4,1], ['+',4,2], ['-',4,3], ['.',5,0]]
+
   
 # specials
 
