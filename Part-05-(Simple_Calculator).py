15e7b0f2ea2caf82144c0c90e85359a714d60a23
4Raisan <mn4raisan@gmail.com>
2025-04-29 23:13:18 +0530

    design the number button places

--- Part-05-(Simple_Calculator).py
+++ Part-05-(Simple_Calculator).py
@@ -11,6 +11,10 @@ e.grid(row=0, column=0)
 # process as defs (+,-,/,*)
 
 # buttons with positions, use grid()
+# numbers
+btnnums = [['1',1,0], ['2',1,1], ['3',1,2], ['4',1,3], ['5',2,0], ['6',2,1], ['7',2,2], ['8',2,3], ['9',3,0], ['0',3,1]]
+  
+# specials
 
 
 root.mainloop()
