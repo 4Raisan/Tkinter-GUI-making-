22b34c1f25a5e1d8d3fcc50100f094f77089d5ac
4Raisan <mn4raisan@gmail.com>
2025-04-30 23:10:29 +0530

    add result = button

--- Part-05-(Simple_Calculator).py
+++ Part-05-(Simple_Calculator).py
@@ -11,6 +11,7 @@ e.grid(row=0, column=0, columnspan=4)
 
 # process as defs (+,-,/,*)
 
+
 # buttons with positions, use grid()
 # numbers
 btnnums = [['1',1,0], ['2',1,1], ['3',1,2], ['4',1,3], ['5',2,0], ['6',2,1], ['7',2,2], ['8',2,3], ['9',3,0], ['0',3,1]]
@@ -18,15 +19,21 @@ for (txt, r, c) in btnnums:
   btn = Button(root, text=txt)
   btn.grid(row=r, column=c)
 # arithmetic operators and .
+
 btnarm = [['*',4,0], ['/',4,1], ['+',4,2], ['-',4,3], ['.',5,0]]
 for (txt, r, c) in btnarm:
   btn = Button(root, text=txt)
   btn.grid(row=r, column=c)
+  
 # results and clears
 buttonclr = Button(root, text='AC')
 buttonclr.grid(row=3, column=2)
+
 buttondel = Button(root, text='⌫')
 buttondel.grid(row=3, column=3)
+
+buttoneql = Button(root, text='=')
+buttoneql.grid(row=5, column=1, columnspan=3)
 
 # specials
 
