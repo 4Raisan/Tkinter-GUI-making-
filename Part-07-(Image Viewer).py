2c7b33e071efae2b107eefd0ff76a9475ace27a9
4Raisan <mn4raisan@gmail.com>
2025-05-21 23:35:49 -1200

    display buttons using grids

--- Part-07-(Image Viewer).py
+++ Part-07-(Image Viewer).py
@@ -2,6 +2,8 @@ from tkinter import *
 root = Tk()
 
 button_next = Button(root, text="<<", command=#)
+button_next.grid(row=1, column=0)                     
 button_back = Button(root, text=">>", command=#)
-
+button_back.grid(row=1, column=1)
+          
 root.mainloop()
