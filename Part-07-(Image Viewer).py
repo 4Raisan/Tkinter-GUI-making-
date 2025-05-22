df42f7ae12942025853684b8be12740f258fbf22
Rashen Anupama <mn4raisan@gmail.com>
2025-05-22 14:23:00 +0530

    images display using pillow

--- Part-07-(Image Viewer).py
+++ Part-07-(Image Viewer).py
@@ -2,13 +2,14 @@ from tkinter import *
 from os import *
 from PIL import ImageTk,Image
 
-
 root = Tk()
 root.title("Image Viewer")
 
 slocation = path.dirname(path.abspath(__file__))
 # make a list of the other files/folders on the same directory
 iteamlist = listdir(slocation)
+
+img1 = ImageTk.PhotoImage(Image.open("Part-06-(Image).png"))
 
 button_next = Button(root, text="<<", command=#)
 button_next.grid(row=1, column=0)                     
