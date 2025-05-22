5e0b4cb0d55ec11a99c8f42fe0d8dfc823abbc0a
Rashen Anupama <mn4raisan@gmail.com>
2025-05-22 14:34:00 +0530

    show images with labels + pillow

--- Part-07-(Image Viewer).py
+++ Part-07-(Image Viewer).py
@@ -10,10 +10,12 @@ slocation = path.dirname(path.abspath(__file__))
 iteamlist = listdir(slocation)
 
 img1 = ImageTk.PhotoImage(Image.open("Part-06-(Image).png"))
+labelI = Label(image=img1)
+labelI.grid(row=0, column=0)
 
-button_next = Button(root, text="<<", command=#)
+'''button_next = Button(root, text="<<", command=#)
 button_next.grid(row=1, column=0)                     
 button_back = Button(root, text=">>", command=#)
-button_back.grid(row=1, column=1)
+button_back.grid(row=1, column=1)'''
           
 root.mainloop()
