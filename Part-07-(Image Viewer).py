43ecfcef34af6d8b301e169ccc032e06a9906786
Rashen Anupama <mn4raisan@gmail.com>
2025-05-24 12:34:00 +0530

    check and support for 8 image formats

--- Part-07-(Image Viewer).py
+++ Part-07-(Image Viewer).py
@@ -23,13 +23,16 @@ def locator(carry):
 slocation = path.dirname(path.abspath(__file__)) # make a list of the other files/folders on the same directory
 iteamlist = listdir(slocation)
 piclist = []
+
+picformats = ["jpeg","tiff","webp",".jpg",".png",".gif",".bmp",".tif"]
 for i in iteamlist:
-    if (str(i)[-3:])=="png":
-        piclist.append(i)
+    for f in picformats:
+        if (i[-4:]==f):
+            piclist.append(i)
 print(piclist)
 
-piclistaccs = 0
 
+piclistaccs = 0
 show(piclistaccs) # 1st image call
 button_next = Button(root, text=">>", command=lambda: (locator("+"), show(piclistaccs)))
 button_next.grid(row=1, column=1)                     
