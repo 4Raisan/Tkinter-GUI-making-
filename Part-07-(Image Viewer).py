54641c6879efd6246ee644c5545aadeceeed7c61
Rashen Anupama <mn4raisan@gmail.com>
2025-05-24 11:58:00 +0530

    fix index range error and improve functions calling by buttons part

--- Part-07-(Image Viewer).py
+++ Part-07-(Image Viewer).py
@@ -5,17 +5,18 @@ from PIL import ImageTk,Image
 root = Tk()
 root.title("Image Viewer")
 
+
 def show(pth):
-    global img, labelimg, inx
+    global img, labelimg
     img = ImageTk.PhotoImage(Image.open(piclist[pth]))
     labelimg = Label(image=img)
     labelimg.grid(columnspan=2, row=0, column=0)
 
 def locator(carry):
     global piclistaccs
-    if carry=='+':
+    if carry=='+' and piclistaccs<(len(piclist)-1):
         piclistaccs+=1
-    else:
+    elif carry=="-" and piclistaccs>0:
         piclistaccs-=1
 
 
@@ -29,10 +30,10 @@ print(piclist)
 
 piclistaccs = 0
 
-show(piclistaccs)
-button_next = Button(root, text="<<", command=lambda: (show(piclistaccs), locator("+")))
-button_next.grid(row=1, column=0)                     
-button_back = Button(root, text=">>", command=lambda: (show(piclistaccs), locator("-")))
-button_back.grid(row=1, column=1)
-          
+show(piclistaccs) # 1st image call
+button_next = Button(root, text=">>", command=lambda: (locator("+"), show(piclistaccs)))
+button_next.grid(row=1, column=1)                     
+button_back = Button(root, text="<<", command=lambda: (locator("-"), show(piclistaccs)))
+button_back.grid(row=1, column=0)
+
 root.mainloop()
