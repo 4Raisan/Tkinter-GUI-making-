66f8c412d2fbb7ae3f21917c97e9197a36011fae
Rashen Anupama <mn4raisan@gmail.com>
2025-06-17 09:52:55 +0530

    Info popup

--- Part-08-(Popup-Messagebox).py
+++ Part-08-(Popup-Messagebox).py
@@ -3,6 +3,10 @@ from tkinter import messagebox
 
 root = tk.Tk()
 
+# 3 Main Messagebox types -------------------------------
+
+# 1. with Info symbole
+messagebox.showinfo("Title", "This is an info popup")
 
 
 root.mainloop()
